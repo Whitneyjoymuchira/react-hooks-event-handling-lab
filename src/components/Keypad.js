550155e9e87bac1150b9b1a9d7386f3117682a57
Whitney Muchira <whitneymuchira@gmail.com>
2022-07-17 10:27:14 +0300

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react"
+export default function Keypad(){
+    function handleEvent(event){
+    console.log('Entering password...')
+    }
+        return (
+            <form onChange={handleEvent}>
+              <input type="password" name="password" placeholder="Enter password..." />
+              <button>Login</button>
+            </form>
+          );
+        }
+
+    
+
